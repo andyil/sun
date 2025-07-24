e3f7cedf4c1c00a0f544fcab5d8ffcd9e1d4b506
andyil <andyworms+github@gmail.com>
2025-07-24 17:34:47 +0300

    Update json2csv.py

--- formats/json2csv.py
+++ formats/json2csv.py
@@ -7,5 +7,5 @@ writer = None
 for line in sys.stdin:
    j = json.loads(line)
    if writer is None:
-     writer = csv.DictWriter(f=sys.stdout, fieldnames=list(k.keys()))
+     writer = csv.DictWriter(f=sys.stdout, fieldnames=list(j.keys()))
    writer.writerow(j.values())
